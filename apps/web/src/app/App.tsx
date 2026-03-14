2f8edecd8de3432aadfd33340191520eb3f4a100
DENISmer <rediskink@inbox.ru>
2026-03-15 01:14:58 +0400

    project simple architecture (for 1 page currently); tsconfig setup; .module.scss fixes

---
+++ apps/web/src/app/App.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/App.module.scss";
+
+export const App = () => {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.title}>HTTPS Request Visualizer</h1>
+    </div>
+  );
+};
